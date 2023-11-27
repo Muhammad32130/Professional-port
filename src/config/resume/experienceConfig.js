69e4c43c9a443a92cb7acd078f7f11126c2a5ded
Muhammad32130 <askari.muhammad321@gmail.com>
2023-11-26 19:01:51 -0600

    Portfolio started

---
+++ src/config/resume/experienceConfig.js
@@ -0,0 +1,13 @@
+export const experienceConfig = [
+  {
+    name: "Summarist",
+    title: "Frontend engineer Intern",
+    description: [
+      "Developed Summarist, a modern web platform, using Firebase, React, TypeScript, and integrated Stripe Checkout for seamless payment processing",
+      "Using React and TypeScript to engineer a frontend codebase using industry best practises",
+      "Writing extremely clean and reusable code by making effective use of dependency injection, code isolation, local reasoning, clean module boundaries, fast development cycles, tests, deterministic behaviour and type safety",
+    ],
+    date: "May 2023 - Dec 2023",
+  },
+  
+];
