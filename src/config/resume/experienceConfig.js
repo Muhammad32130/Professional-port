6a21f6b51c029bc9f2f037adba9f0b427d60c02b
Muhammad32130 <askari.muhammad321@gmail.com>
2023-12-18 15:30:08 -0600

    Experience Update

--- src/config/resume/experienceConfig.js
+++ src/config/resume/experienceConfig.js
@@ -9,5 +9,16 @@ export const experienceConfig = [
     ],
     date: "May 2023 - Dec 2023",
   },
+  {
+    name: "Scaling.so",
+    title: "Frontend Developer",
+    description: [
+      "Implemented OAuth integration for Calendly using its API, ensuring seamless token management by utilizing refresh tokens for extended sessions beyond the initial 2-hour OAuth validity",
+      "Advanced the user experience through dynamic route changes based on actions like date sorting and event history navigation, fortified by user-centric URL structuring",
+      "Engineered a robust backend API using Node.js and Express, ensuring scalability, flexibility, and seamless frontend interaction",
+      "Designed and integrated charts to efficiently represent statistical data, aiding in better user comprehension and decision-making",
+    ],
+    date: "Jan 2022 - Feb 2023",
+  },
   
 ];
